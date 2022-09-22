98f2e862d5c51cace5b68f51abd081b9b2fff625
Omachi John <omachi.john@.com>
2022-09-22 01:33:10 +0100

    Generate user id to make unique async call

--- src/redux/books/DisplayBooks.js
+++ src/redux/books/DisplayBooks.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { asyncRemove } from "./apiThunks";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { asyncRemove } from './apiThunks';
 
-import Book from "./Book";
+import Book from './Book';
 // import Form from './Form';
-import NavBar from "./NavBar";
-import Form from "./apiform";
+import NavBar from './NavBar';
+import Form from './apiform';
 // import { removeBook } from "./books";
 
 function DisplayBooks() {
--- src/redux/books/apiThunks.js
+++ src/redux/books/apiThunks.js
@@ -1,31 +1,31 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import {
-  addBookToAPI,
-  getStateFromAPI,
-  removeBookFromAPI,
-} from '../../component/api/actioApi';
-import {
-  GET, ASYNC_ADD, ASYNC_REMOVE, addBook, removeBook,
-} from './apiBooks';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import {
+//   addBookToAPI,
+//   getStateFromAPI,
+//   removeBookFromAPI,
+// } from '../../component/api/actioApi';
+// import {
+//   GET, ASYNC_ADD, ASYNC_REMOVE, addBook, removeBook,
+// } from './apiBooks';
 
-const asyncAdd = createAsyncThunk(ASYNC_ADD, async (book, { dispatch }) => {
-  const answer = await addBookToAPI(book);
-  dispatch(addBook(book));
-  return answer;
-});
+// const asyncAdd = createAsyncThunk(ASYNC_ADD, async (book, { dispatch }) => {
+//   const answer = await addBookToAPI(book);
+//   dispatch(addBook(book));
+//   return answer;
+// });
 
-const asyncRemove = createAsyncThunk(
-  ASYNC_REMOVE,
-  async (book, { dispatch }) => {
-    const answer = await removeBookFromAPI(book);
-    dispatch(removeBook(book));
-    return answer;
-  },
-);
+// const asyncRemove = createAsyncThunk(
+//   ASYNC_REMOVE,
+//   async (book, { dispatch }) => {
+//     const answer = await removeBookFromAPI(book);
+//     dispatch(removeBook(book));
+//     return answer;
+//   },
+// );
 
-const asyncGet = createAsyncThunk(GET, async () => {
-  const answer = await getStateFromAPI();
-  return answer.books;
-});
+// const asyncGet = createAsyncThunk(GET, async () => {
+//   const answer = await getStateFromAPI();
+//   return answer.books;
+// });
 
-export { asyncAdd, asyncGet, asyncRemove };
+// export { asyncAdd, asyncGet, asyncRemove };
