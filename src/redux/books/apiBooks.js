f2a8b2fec47f3a46685e80100fc84eb3755c7a1a
Omachi John <omachi.john@.com>
2022-09-22 01:19:30 +0100

    Add actions for API calls

--- src/redux/books/apiBooks.js
+++ src/redux/books/apiBooks.js
@@ -1,57 +1,47 @@
-// const ADD = 'bookstore/books/ADD';
-// const REMOVE = 'bookstore/books/REMOVE';
-// const UPDATE = 'bookstore/books/UPDATE';
-// const GET = 'bookstore/books/GET';
-// const ASYNC_ADD = 'bookstore/books/ASYNC_ADD';
-// const ASYNC_REMOVE = 'bookstore/books/ASYNC_REMOVE';
+const ADD = "bookstore/books/ADD";
+const REMOVE = "bookstore/books/REMOVE";
+const UPDATE = "bookstore/books/UPDATE";
+const GET = "bookstore/books/GET";
+const ASYNC_ADD = "bookstore/books/ASYNC_ADD";
+const ASYNC_REMOVE = "bookstore/books/ASYNC_REMOVE";
 
-// const addBook = (book) => ({
-//   type: ADD,
-//   book,
-// });
+const addBook = (book) => ({
+  type: ADD,
+  book,
+});
 
-// const removeBook = (book) => ({
-//   type: REMOVE,
-//   book,
-// });
+const removeBook = (book) => ({
+  type: REMOVE,
+  book,
+});
 
-// const updateBook = (book) => ({
-//   type: UPDATE,
-//   book,
-// });
+const updateBook = (book) => ({
+  type: UPDATE,
+  book,
+});
 
-// const bookReducer = (state = [], action = {}) => {
-//   switch (action.type) {
-//     case ADD:
-//       if (state.find((elem) => elem.item_id === action.book.item_id)) {
-//         return state;
-//       }
-//       return [
-//         ...state,
-//         action.book,
-//       ];
-//     case UPDATE:
-//       return state.map((book) => {
-//         if (book.item_id === action.book.item_id) {
-//           return action.book;
-//         }
-//         return book;
-//       });
-//     case REMOVE:
-//       return state.filter((book) => book.item_id !== action.book.item_id);
-//     case `${GET}/fulfilled`:
-//       return action.payload;
-//     default:
-//       return state;
-//   }
-// };
+const bookReducer = (state = [], action = {}) => {
+  switch (action.type) {
+    case ADD:
+      if (state.find((elem) => elem.item_id === action.book.item_id)) {
+        return state;
+      }
+      return [...state, action.book];
+    case UPDATE:
+      return state.map((book) => {
+        if (book.item_id === action.book.item_id) {
+          return action.book;
+        }
+        return book;
+      });
+    case REMOVE:
+      return state.filter((book) => book.item_id !== action.book.item_id);
+    case `${GET}/fulfilled`:
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
-// export {
-//   addBook,
-//   updateBook,
-//   removeBook,
-//   GET,
-//   ASYNC_ADD,
-//   ASYNC_REMOVE,
-// };
-// export default bookReducer;
+export { addBook, updateBook, removeBook, GET, ASYNC_ADD, ASYNC_REMOVE };
+export default bookReducer;
