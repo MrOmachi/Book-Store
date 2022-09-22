1efbf92fdbb49a6608d3fd3eb4c29cb4db332c39
Omachi John <omachi.john@.com>
2022-09-22 01:35:38 +0100

    Render fetched datas

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,23 +1,23 @@
-// import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-// export const bookSlice = createSlice({
-//   name: 'books',
-//   initialState: {
-//     value: [
-//       { id: 1, author: 'By authot one', title: 'Book one' },
-//       { id: 2, author: 'By authot two', title: 'Book two' },
-//     ],
-//   },
-//   reducers: {
-//     addBook: (state, action) => {
-//       state.value.push(action.payload);
-//     },
+export const bookSlice = createSlice({
+  name: "books",
+  initialState: {
+    value: [
+      { id: 1, author: "By authot one", title: "Book one" },
+      { id: 2, author: "By authot two", title: "Book two" },
+    ],
+  },
+  reducers: {
+    addBook: (state, action) => {
+      state.value.push(action.payload);
+    },
 
-//     removeBook: (state, action) => {
-//       state.value = state.value.filter((book) => book.id !== action.payload.id);
-//     },
-//   },
-// });
+    removeBook: (state, action) => {
+      state.value = state.value.filter((book) => book.id !== action.payload.id);
+    },
+  },
+});
 
-// export const { addBook, removeBook } = bookSlice.actions;
-// export default bookSlice.reducer;
+export const { addBook, removeBook } = bookSlice.actions;
+export default bookSlice.reducer;
